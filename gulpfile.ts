868d9d8a390b74566aa5efc24ef4a308b978430f
den <denvstheworld@gmail.com>
2022-07-18 16:02:02 -0400

    Update gulpfile.ts

--- gulpfile.ts
+++ gulpfile.ts
@@ -6,12 +6,12 @@ let name = JSON.parse(fs.readFileSync("./package.json").toString()).name;
 
 gulp.task('build', function () {
     try {
-        let meta = JSON.parse(fs.readFileSync(`./cores/${name}/package.json`).toString());
+        let meta = JSON.parse(fs.readFileSync(`./src/${name}/package.json`).toString());
         meta.date = new Date().toUTCString();
         meta.commit = child_process.execSync("git rev-parse --short HEAD").toString().replace("\n", "");
         meta.version = meta.version.split("-")[0];
         meta.version = meta.version + `-nightly@${meta.commit}`;
-        fs.writeFileSync(`./cores/${name}/package.json`, JSON.stringify(meta, null, 2));
+        fs.writeFileSync(`./src/${name}/package.json`, JSON.stringify(meta, null, 2));
         child_process.execSync('npx tsc');
     } catch (err: any) {
         console.log(err.stack);
