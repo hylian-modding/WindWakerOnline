8f633b7714001e877e7dc3e4ceb48b702df6ba7a
Psi-Hate <dianaelzascudder@gmail.com>
2023-01-15 11:20:02 -0600

    conda stuff

---
+++ conda/bld.bat
@@ -0,0 +1,3 @@
+mkdir %PREFIX%\client
+mkdir %PREFIX%\client\mods
+copy %cd%\WindWakerOnline\WindWakerOnline.pak %PREFIX%\client\mods\WindWakerOnline.pak
---
+++ conda/make_conda.bat
@@ -0,0 +1,4 @@
+set CONDA_BLD_PATH=D:\Modding\ML64\modloader64.com\
+set MYPKG_VERSION=1.0.0
+set MYPKG_BUILD=1
+conda build .
